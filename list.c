037f3f308f9c14133fb2a19fe6b877d73a4cda67
schneider <schneider@office-ubuntu>
2012-06-29 10:40:36 +0800

    add list's destroy method.

--- list.c
+++ list.c
@@ -8,18 +8,26 @@ struct LIST {
 	ElemType a;
 	ElemType b;
 	ElemType c;
-	struct LIST *next;
+//	struct LIST *next;
 };
 
-int initList(struct LIST *list)
+int initList(struct LIST **list)
 {
-	list = (struct LIST *)malloc(sizeof(struct LIST));
-	if(!list) {
+	*list = (struct LIST *)malloc(sizeof(struct LIST));
+	if(!*list) {
 		printf("no enough memory.\n");
 		return -1;
 	}
 
+	printf("%s: list's addr:%p\n", __func__, list);
+	printf("%s: list's value:%p\n", __func__, *list);
 	return 0;
+}
+
+void destroyList(struct LIST *list)
+{
+	free((void*)list);
+	list = NULL;
 }
 
 int main()
@@ -27,11 +35,16 @@ int main()
 	struct LIST *list;
 	int ret;
 
-	ret = initList(list);
-	if(ret)
-		printf("initList failed\n");
+	list = NULL;
+	printf("the pointer list's addr:%p\n", &list);
+	printf("the pointer list's value after NULL:%p\n", list);
 
-
+	ret = initList(&list);
+	if(!ret) {
+		printf("the pointer list's addr:%p\n", &list);
+		printf("the pointer list's value after init:%p\n", list);
+		destroyList(list);
+	}
 
 	return 0;
 }
