0d794cffc108dc692d0f4c729a93e5d4f06aa2e3
Xiaodong Wang <wangxd@marvell.com>
2013-04-19 17:54:29 +0800

    [data

---
+++ ds/ch5_glist/array.h
@@ -0,0 +1,9 @@
+#include <stdarg.h>
+
+#define MAX_ARRAY_DIM 8
+typedef struct {
+	ElemType *base;
+	int dim;
+
+	
+}Array;
---
+++ ds/ch5_glist/array_test.c
@@ -0,0 +1,6 @@
+#include "array.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
